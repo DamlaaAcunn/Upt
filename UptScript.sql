a88ffe66063b9ecb1c27e06560f66fb1ff6a22e6
DamlaaAcunn <38463245+DamlaaAcunn@users.noreply.github.com>
2021-02-26 16:02:05 +0300

    Add files via upload

---
+++ UptScript.sql
@@ -0,0 +1,199 @@
+CREATE TABLE [dbo].[Customers](
+	[CustomerID] [int] IDENTITY(1,1) NOT NULL,
+	[UserID] [int]  NOT NULL,
+	[Name] [nvarchar](128) NOT NULL,
+	IdentityNumber nvarchar(20),
+	[CustomerTypeID] [int] NOT NULL,
+	[CustomerStatusID] [int] NOT NULL,
+	[Email] [varchar](64) NOT NULL,
+	[Phone] [varchar](32) NOT NULL,
+	[TaxNumber] [varchar](32)  NULL,
+	[Address] [nvarchar](256) NOT NULL,
+	[Register] [datetime] NOT NULL,
+ CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED 
+(
+	[CustomerID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+CREATE TABLE [dbo].[CustomerStatuses](
+	[CustomerStatusID] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerStatus] [nvarchar](64) NOT NULL,
+ CONSTRAINT [PK_CustomerStatuses] PRIMARY KEY CLUSTERED 
+(
+	[CustomerStatusID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+CREATE TABLE [dbo].[InvoiceLines](
+	[InvoiceLineID] [int] IDENTITY(1,1) NOT NULL,
+	[Description] [nvarchar](128) NOT NULL,
+	[Unit] [nvarchar](16) NOT NULL,
+	[Price] [decimal](17, 4) NOT NULL,
+	[VatRatio] [decimal](17, 4) NOT NULL,
+	[InvoiceID] [int] NOT NULL,
+ CONSTRAINT [PK_InvoiceLines] PRIMARY KEY CLUSTERED 
+(
+	[InvoiceLineID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+CREATE TABLE [dbo].[Invoices](
+	[InvoiceID] [int] IDENTITY(1,1) NOT NULL,
+	[InvoiceNumber] [varchar](16) NOT NULL,
+	[Title] [nvarchar](64) NOT NULL,
+	[CustomerID] [int] NOT NULL,
+	[Address] [nvarchar](256) NOT NULL,
+	[InvoiceStatusID] [int] NOT NULL,
+	[InvoiceTypeID] [int] NOT NULL,
+	[PaymentTypeID] [int] NOT NULL,
+	[PaymentDate] [date] NOT NULL,
+	[PaidDate] [date] NULL,
+	[CreateDateTime] [datetime] NOT NULL,
+	[UserID] [int] NOT NULL,
+	[Price] [decimal(30,2)] not null
+ CONSTRAINT [PK_Invoices] PRIMARY KEY CLUSTERED 
+(
+	[InvoiceID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+CREATE TABLE [dbo].[InvoiceStatuses](
+	[InvoiceStatusID] [int] IDENTITY(1,1) NOT NULL,
+	[InvoiceStatus] [nvarchar](32) NOT NULL,
+ CONSTRAINT [PK_InvoiceStatus] PRIMARY KEY CLUSTERED 
+(
+	[InvoiceStatusID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+CREATE TABLE [dbo].[InvoiceTypes](
+	[InvoiceTypeID] [int] IDENTITY(1,1) NOT NULL,
+	[InvoiceType] [nvarchar](32) NOT NULL,
+ CONSTRAINT [PK_InvoiceType] PRIMARY KEY CLUSTERED 
+(
+	[InvoiceTypeID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+CREATE TABLE [dbo].[PaymentTypes](
+	[PaymentTypeID] [int] IDENTITY(1,1) NOT NULL,
+	[PaymentType] [nvarchar](32) NOT NULL,
+ CONSTRAINT [PK_PaymentTypes] PRIMARY KEY CLUSTERED 
+(
+	[PaymentTypeID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+CREATE TABLE [dbo].[SecurityKeys](
+	[KeyID] [int] IDENTITY(1,1) NOT NULL,
+	[Key] [varchar](64) NOT NULL,
+	[UserID] [int] NOT NULL,
+	[Expire] [datetime] NOT NULL,
+ CONSTRAINT [PK_SecurityKeys] PRIMARY KEY CLUSTERED 
+(
+	[KeyID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+CREATE TABLE [dbo].[UserRoles](
+	[RoleID] [int] IDENTITY(1,1) NOT NULL,
+	[RoleName] [nvarchar](32) NOT NULL,
+ CONSTRAINT [PK_UserRoles] PRIMARY KEY CLUSTERED 
+(
+	[RoleID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+CREATE TABLE [dbo].[Users](
+	[UserID] [int] IDENTITY(1,1) NOT NULL,
+	[UserName] [varchar](32) NOT NULL,
+	[Password] [varbinary](max) NOT NULL,
+	[RoleID] [int] NOT NULL,
+	[Register] [datetime] NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[UserID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+CREATE TABLE [dbo].[Deposit](
+	[DepositID] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerID] [int] NOT NULL,
+	[Price] decimal(30,2) null,
+	[IsPayment] bit not null,
+	[PaidDate] datetime null
+
+ CONSTRAINT [PK_Deposit] PRIMARY KEY CLUSTERED 
+(
+	[DepositID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+ALTER TABLE [dbo].[Customers]  WITH CHECK ADD  CONSTRAINT [FK_Customers_Users] FOREIGN KEY([UserID])
+REFERENCES [dbo].[Users] ([UserID])
+GO
+
+ALTER TABLE [dbo].[Deposit]  WITH CHECK ADD  CONSTRAINT [FK_Deposit_Customer] FOREIGN KEY([CustomerID])
+REFERENCES [dbo].[Customers] ([CustomerID])
+go 
+
+ALTER TABLE [dbo].[Customers]  WITH CHECK ADD  CONSTRAINT [FK_Customers_CustomerStatuses] FOREIGN KEY([CustomerStatusID])
+REFERENCES [dbo].[CustomerStatuses] ([CustomerStatusID])
+GO
+ALTER TABLE [dbo].[Customers] CHECK CONSTRAINT [FK_Customers_CustomerStatuses]
+GO
+ALTER TABLE [dbo].[Customers]  WITH CHECK ADD  CONSTRAINT [FK_Customers_CustomerTypes] FOREIGN KEY([CustomerTypeID])
+REFERENCES [dbo].[CustomerTypes] ([CustomerTypeID])
+GO
+ALTER TABLE [dbo].[Customers] CHECK CONSTRAINT [FK_Customers_CustomerTypes]
+GO
+ALTER TABLE [dbo].[InvoiceLines]  WITH CHECK ADD  CONSTRAINT [FK_InvoiceLines_Invoices] FOREIGN KEY([InvoiceID])
+REFERENCES [dbo].[Invoices] ([InvoiceID])
+GO
+ALTER TABLE [dbo].[InvoiceLines] CHECK CONSTRAINT [FK_InvoiceLines_Invoices]
+GO
+ALTER TABLE [dbo].[Invoices]  WITH CHECK ADD  CONSTRAINT [FK_Invoices_Customers] FOREIGN KEY([CustomerID])
+REFERENCES [dbo].[Customers] ([CustomerID])
+GO
+ALTER TABLE [dbo].[Invoices] CHECK CONSTRAINT [FK_Invoices_Customers]
+GO
+ALTER TABLE [dbo].[Invoices]  WITH CHECK ADD  CONSTRAINT [FK_Invoices_InvoiceStatuses] FOREIGN KEY([InvoiceStatusID])
+REFERENCES [dbo].[InvoiceStatuses] ([InvoiceStatusID])
+GO
+ALTER TABLE [dbo].[Invoices] CHECK CONSTRAINT [FK_Invoices_InvoiceStatuses]
+GO
+ALTER TABLE [dbo].[Invoices]  WITH CHECK ADD  CONSTRAINT [FK_Invoices_InvoiceTypes] FOREIGN KEY([InvoiceTypeID])
+REFERENCES [dbo].[InvoiceTypes] ([InvoiceTypeID])
+GO
+ALTER TABLE [dbo].[Invoices] CHECK CONSTRAINT [FK_Invoices_InvoiceTypes]
+GO
+ALTER TABLE [dbo].[Invoices]  WITH CHECK ADD  CONSTRAINT [FK_Invoices_PaymentTypes] FOREIGN KEY([PaymentTypeID])
+REFERENCES [dbo].[PaymentTypes] ([PaymentTypeID])
+GO
+ALTER TABLE [dbo].[Invoices] CHECK CONSTRAINT [FK_Invoices_PaymentTypes]
+GO
+ALTER TABLE [dbo].[Invoices]  WITH CHECK ADD  CONSTRAINT [FK_Invoices_Users] FOREIGN KEY([UserID])
+REFERENCES [dbo].[Users] ([UserID])
+GO
+ALTER TABLE [dbo].[Invoices] CHECK CONSTRAINT [FK_Invoices_Users]
+GO
+ALTER TABLE [dbo].[SecurityKeys]  WITH CHECK ADD  CONSTRAINT [FK_SecurityKeys_Users] FOREIGN KEY([UserID])
+REFERENCES [dbo].[Users] ([UserID])
+GO
+ALTER TABLE [dbo].[SecurityKeys] CHECK CONSTRAINT [FK_SecurityKeys_Users]
+GO
+ALTER TABLE [dbo].[Users]  WITH CHECK ADD  CONSTRAINT [FK_Users_UserRoles] FOREIGN KEY([RoleID])
+REFERENCES [dbo].[UserRoles] ([RoleID])
+GO
+ALTER TABLE [dbo].[Users] CHECK CONSTRAINT [FK_Users_UserRoles]
+GO
+USE [UPT]
+GO
+ALTER DATABASE [UPT] SET  READ_WRITE 
+GO
